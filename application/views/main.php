3f2e6b1622f33126e767128041e8ff457754fe03
cadavez <cadavez@gmail.com>
2019-03-19 19:56:25 +0800

    First Commit

---
+++ application/views/main.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Main</title>
+	<link rel="stylesheet" type="text/css" href="assets/css/style.css">
+</head>
+<body>
+	<h1> My Views </h1>
+	<h4> SAMPLE GIT </h4>
+</body>
+	<script src="assets/js/jquery.js"></script>
+</html>
